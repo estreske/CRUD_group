91d16cd6f71cf1b6afe258f9f97243888c26e8d5
Eric Streske <estreske@gmail.com>
2014-01-27 14:41:00 -0500

    first commit

---
+++ app/controllers/dinosaurs_controller.rb
@@ -0,0 +1,21 @@
+class DinosaursController < ApplicationContoller
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+  
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CRUDGroup::Application.config.secret_token = '33a5cd34359e3db320a61b2ab439cfa52ba06cd621e550eed091c2a666e5a67d895e7bc6014eaaf1cda2051d39baa5c087450dbf7f1ecefd8fa0a04f22485fe7'
